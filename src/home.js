13e20a6f0ffe36a0dd01b4b46231ea5034b6385c
gweryf <priyam2007sp@gmail.com>
2023-01-10 11:29:02 +0530

    added basic todo list structure

--- src/home.js
+++ src/home.js
@@ -54,13 +54,72 @@ function createSidebar(){
 
 function createCanvas(){
     const canvas = document.createElement('div')
-    canvas.classList.add('canvas')
+    canvas.classList.add('todo-list')
 
-    const createTaskbut = document.createElement('button')
-    createTaskbut.classList.add('createTaskbut')
-    createTaskbut.textContent = '+ Add Task'
+    // the to-do section will contain of 3 parts - the project DataTransferItemList, tasks and options to clear or delete the tasks
+    //header
+    const todohead = document.createElement('div')
+    todohead.classList.add('todohead')
 
-    canvas.appendChild(createTaskbut)
+    const todotit = document.createElement('h2')
+    todotit.classList.add('list-titte')
+    todotit.textContent = 'Sample 1'
+
+    todohead.appendChild(todotit)
+    canvas.appendChild(todohead)
+
+    //the body
+    const todobod = document.createElement('div')
+    todobod.classList.add('todo-body')
+
+    const tasks = document.createElement('div')
+    tasks.classList.add('tasks')
+
+    const task1 = document.createElement('div')
+    task1.classList.add('task')
+    const task2 = document.createElement('div')
+    task2.classList.add('task')
+    const task3 = document.createElement('div')
+    task3.classList.add('task')
+
+    const input1 = document.createElement('input')
+    input1.type = 'checkbox'
+    input1.id = 'task-1'
+
+    const label_1 = document.createElement('label')
+    label_1.textContent = "Make a to-do List Web App" 
+    label_1.for = 'task-1'
+
+    const input2 = document.createElement('input')
+    input2.type = 'checkbox'
+    input2.id = 'task-2'
+
+    const label_2 = document.createElement('label')
+    label_2.textContent = "Make a restaurant page" 
+    label_2.for = 'task-2'
+
+    const input3 = document.createElement('input')
+    input3.type = 'checkbox'
+    input3.id = 'task-3'
+
+    const label_3 = document.createElement('label')
+    label_3.textContent = "Make a sketching web app" 
+    label_3.for = 'task-3'
+
+    task1.appendChild(input1)
+    task1.appendChild(label_1)
+    task2.appendChild(input2)
+    task2.appendChild(label_2)
+    task3.appendChild(input3)
+    task3.appendChild(label_3)
+
+    tasks.appendChild(task1)
+    tasks.appendChild(task2)
+    tasks.appendChild(task3)
+
+    todobod.appendChild(tasks)
+    canvas.appendChild(todobod)
+
     return canvas
 }
 
